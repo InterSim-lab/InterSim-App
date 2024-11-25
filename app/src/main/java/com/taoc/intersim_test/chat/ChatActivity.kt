01c65e76144a30efd66b83218116a32fceda2410
malzrsyd <akmalmr52@gmail.com>
2024-11-26 03:51:55 +0700

    Create ChatActivity

---
+++ app/src/main/java/com/taoc/intersim_test/chat/ChatActivity.kt
@@ -0,0 +1,27 @@
+package com.taoc.intersim_test.chat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.taoc.intersim_app.R
+import com.taoc.intersim_test.ui.detailjobs.DetailJobsActivity
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, DetailJobsActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/taoc/intersim_test/ui/detailjobs/DetailJobsActivity.kt
+++ app/src/main/java/com/taoc/intersim_test/ui/detailjobs/DetailJobsActivity.kt
@@ -1,21 +1,25 @@
 package com.taoc.intersim_test.ui.detailjobs
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.taoc.intersim_app.R
+import com.taoc.intersim_test.chat.ChatActivity
 
 class DetailJobsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_detail_jobs)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val enterChatButton: Button = findViewById(R.id.enterChatButton)
+        enterChatButton.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
         }
     }
 }
